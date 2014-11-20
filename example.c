32f83943bb6d29961f59154fb094a3221a32cc68
Max Bernstein <tekknolagi@gmail.com>
2014-11-20 00:22:29 -0500

    magic number instead of 0x777

--- example.c
+++ example.c
@@ -9,6 +9,8 @@ extern uint32_t murmur3_32 (const char *, uint32_t, uint32_t);
 extern Fnv32_t fnv_32_str (char *str, Fnv32_t hval);
 extern uint32_t hashlittle (const void *, size_t, uint32_t);
 
+static const bsize_t magic_number = 0x777;
+
 bsize_t hash1 (void *key, bsize_t len);
 bsize_t hash2 (void *key, bsize_t len);
 bsize_t hash3 (void *key, bsize_t len);
@@ -27,7 +29,7 @@ int main () {
 }
 
 bsize_t hash1 (void *key, bsize_t len) {
-  return (bsize_t) murmur3_32((const char *) key, (uint32_t) len, 0x777) % len;
+  return (bsize_t) murmur3_32((const char *) key, (uint32_t) len, magic_number) % len;
 }
 
 bsize_t hash2 (void *key, bsize_t len) {
@@ -35,5 +37,5 @@ bsize_t hash2 (void *key, bsize_t len) {
 }
 
 bsize_t hash3 (void *key, bsize_t len) {
-  return (bsize_t) hashlittle((const void *) key, strlen((const char *) key), 0x777) % len;
+  return (bsize_t) hashlittle((const void *) key, strlen((const char *) key), magic_number) % len;
 }
