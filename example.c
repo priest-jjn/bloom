026229021a71d9ca7fcfefda509f20aa05a0a9ec
Max Bernstein <tekknolagi@gmail.com>
2014-11-25 13:51:45 -0500

    add simplifier functions

--- example.c
+++ example.c
@@ -11,30 +11,41 @@ extern uint32_t hashlittle (const void *, size_t, uint32_t);
 
 static const bsize_t magic_number = 0x777;
 
+void add (bloom_filter_t filter, void *key);
+bloom_result_e query (bloom_filter_t filter, void *key);
+
 bsize_t hash1 (void *key, bsize_t len);
 bsize_t hash2 (void *key, bsize_t len);
 bsize_t hash3 (void *key, bsize_t len);
 
 int main () {
   bloom_filter_t filter = bloom_init(16);
-  bloom_add_multiple(filter, "chris", 3, hash1, hash2, hash3);
-  bloom_add_multiple(filter, "gregg", 3, hash1, hash2, hash3);
-  bloom_add_multiple(filter, "is", 3, hash1, hash2, hash3);
-  bloom_add_multiple(filter, "mayonnaise", 3, hash1, hash2, hash3);
+  add(filter, "chris");
+  add(filter, "gregg");
+  add(filter, "is");
+  add(filter, "mayonnaise");
 
   printf("query chris? %d\n",
-	 bloom_query_multiple(filter, "chris", 3, hash1, hash2, hash3));
+	 query(filter, "chris"));
 
   printf("query gregg? %d\n",
-	 bloom_query_multiple(filter, "gregg", 3, hash1, hash2, hash3));
+	 query(filter, "gregg"));
 
   printf("query is? %d\n",
-	 bloom_query_multiple(filter, "is", 3, hash1, hash2, hash3));
+	 query(filter, "is"));
 
   printf("query mustard? %d\n",
-	 bloom_query_multiple(filter, "mustard", 3, hash1, hash2, hash3));
+	 query(filter, "mustard"));
 
   bloom_destroy(filter);
+}
+
+void add (bloom_filter_t filter, void *key) {
+  bloom_add_multiple(filter, key, 3, hash1, hash2, hash3);
+}
+
+bloom_result_e query (bloom_filter_t filter, void *key) {
+  return bloom_query_multiple(filter, key, 3, hash1, hash2, hash3);
 }
 
 bsize_t hash1 (void *key, bsize_t len) {
